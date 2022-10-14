0484cdb0e6b126ae11ce7cbcd4ea01c7d7b7d4b5
Mohammad Moradi <MrMohammad.Moradi@yahoo.com>
2022-10-14 15:54:33 +0330

    import bootstap and navbar component to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import 'bootstrap/dist/js/bootstrap';
+
+import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar/>
       </header>
     </div>
   );
