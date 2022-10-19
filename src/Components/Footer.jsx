02cba7fd88c1920780077b0bafff0fdd67dab999
Mohammad Moradi <MrMohammad.Moradi@yahoo.com>
2022-10-20 02:22:26 +0330

    change data and tag in footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
     return (
         <div className="Footer">
-            <div className="cantactus d-flex justify-content-center">
-                <div className="frontEnd m-4">
+            <div className="cantactus d-flex justify-content-center pt-4">
+                <div className="frontEnd m-4 mx-2">
                     <h4 className="" id="">
                         برنامه نویس فرانت اند : محمد مرادی
                     </h4>
@@ -15,18 +15,18 @@ function Footer() {
                     <a href="" className="emailProgrammer">
                         MrMohammad.Moradi@yahoo.com
                     </a>
-                    <div className="socialMedia">
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                    <div className="socialMedia p-2">
+                        <a href="https://instagram.com/mrmohammadmoradi_developer" className="instagram">
+                            <i class="bi bi-instagram"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="https://t.me/mr_mmoradi" className="instagram">
+                            <i class="bi bi-telegram"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="https://www.linkedin.com/in/mr-mohammad-moradi/" className="instagram">
+                            <i class="bi bi-linkedin"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="https://github.com/MrMohammadMoradi/" className="instagram">
+                            <i class="bi bi-github"></i>
                         </a>
                     </div>
                 </div>
@@ -37,21 +37,23 @@ function Footer() {
                     </h4>
                     <p className="" id="">
                         دانشجوی کارشناسی مهندسی کامپیوتر مجتمع آموزش عالی
-                        لارستان ورودی 98
+                        لارستان ورودی 99
                     </p>
-                    <a href="" className="emailProgrammer"></a>
-                    <div className="socialMedia">
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                    <a href="" className="emailProgrammer">
+                        Majidrashedipour@gmail.com
+                    </a>
+                    <div className="socialMedia p-2">
+                        <a href="http://instagram.com/majidrashedipour" className="instagram">
+                            <i class="bi bi-instagram"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="http://telegram.me/majidrashedipour" className="instagram">
+                            <i class="bi bi-telegram"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="http://linkedin.com/in/majidrashedipour" className="instagram">
+                            <i class="bi bi-linkedin"></i>
                         </a>
-                        <a href="http://instagram.com" className="instagram">
-                            insta
+                        <a href="http://github.com/majidrashedipour" className="instagram">
+                            <i class="bi bi-github"></i>
                         </a>
                     </div>
                 </div>
