23397b8ef48e2fb287abfe80849f1c7f09ca5c33
Mohammad Moradi <MrMohammad.Moradi@yahoo.com>
2022-10-27 01:59:52 +0330

    Add carousel to page and change style in css

--- src/Pages/HomePaje.jsx
+++ src/Pages/HomePaje.jsx
@@ -1,66 +1,66 @@
-import React from 'react'
+import React from "react";
+
+import { Carousel } from "react-bootstrap";
 
 function HomePaje() {
+    let base = "/assets/images/";
+    let img = base + "logo1.jpg";
+    
+
+
     return (
-        <div className="HomePaje">
-            {/* 
-                import Carousel from 'react-bootstrap/Carousel';
+        <div className="HomePage">
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 CRImage"
+                        src={img}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>
+                            Nulla vitae elit libero, a pharetra augue mollis
+                            interdum.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 CRImage"
+                        src={img}
+                        alt="Second slide"
+                    />
 
-function UncontrolledExample() {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 CRImage"
+                        src={img}
+                        alt="Third slide"
+                    />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl
+                            consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
-
-export default UncontrolledExample;
-            */}
-
-            <h2 className="PopularGames mt-5 mx-3">
-                بازی های محبوب
-            </h2>
-            <div className="gamePopularList">
-
-            </div>
+            
         </div>
     );
 }
 
-export default HomePaje
+export default HomePaje;
