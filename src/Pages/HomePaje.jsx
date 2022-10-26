be340f3bd7c8b2333575ea503a349cc5feaedfd4
Mohammad Moradi <MrMohammad.Moradi@yahoo.com>
2022-10-27 02:05:57 +0330

    Add data and game card to home page and change style in css

--- src/Pages/HomePaje.jsx
+++ src/Pages/HomePaje.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 import { Carousel } from "react-bootstrap";
+import GameCard from "../Components/GameCard";
 
 function HomePaje() {
     let base = "/assets/images/";
     let img = base + "logo1.jpg";
-    
-
+    const Games = [
+        { id: 0, image: img, gameName: "game1 ", description: "0 " },
+        { id: 1, image: img, gameName: "game2 ", description: "1 " },
+        { id: 2, image: img, gameName: "game2 ", description: "2 " },
+        { id: 3, image: img, gameName: "game4 ", description: "3 " },
+        { id: 4, image: img, gameName: "game5 ", description: "4 " },
+        { id: 5, image: img, gameName: "game6 ", description: "5 " },
+        { id: 6, image: img, gameName: "game7 ", description: "6 " },
+        { id: 7, image: img, gameName: "game8 ", description: "7 " },
+        { id: 8, image: img, gameName: "game9 ", description: "8 " },
+        { id: 9, image: img, gameName: "game10 ", description: "9 " },
+    ];
 
     return (
         <div className="HomePage">
@@ -56,9 +67,17 @@ function HomePaje() {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            
 
-            
+            <h2 className="PopularGames my-5 mx-5">بازی های محبوب</h2>
+            <div className="gamePopularList d-flex flex-wrap border border-primary mx-5 mb-5 p-4">
+                {Games.map((item) => (
+                    <GameCard
+                        image={item.image}
+                        gameName={item.gameName}
+                        description={item.description}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
